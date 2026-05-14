518c665481b50c026a77a500c69b806c79d3266d
Nicolas Vidinha <nicolasvidinha2@gmail.com>
2026-05-13 21:30:49 -0300

    implementa primeira opcao

--- main.py
+++ main.py
@@ -11,6 +11,40 @@ while True:
 
     opcao = input("\n Escolha uma opção: ")
 
+    if opcao == "1":
+        produto = input("Nome do produto: ")
+        valor_unitario = float(input("Valor unitário: "))
+        quantidade = int(input("Quantidade: "))
+
+        valor_bruto = valor_unitario * quantidade
+
+        print(f"Nome do produto: {produto}")
+        print(f"Valor unitário: R$ {valor_unitario:.2f}")
+        print(f"Quantidade: {quantidade}")
+
+        print(f"Valor bruto da venda: R$ {valor_bruto:.2f}")
+
+        percentual_desc = 0
+        if valor_bruto < 100:
+            percentual_desc = 0
+        elif 100 > valor_bruto < 499.99:
+            percentual_desc = 5
+        elif 500 > valor_bruto < 999.99:
+            percentual_desc = 10
+        else:
+            percentual_desc = 15
+
+        valor_desconto = (percentual_desc / 100) * valor_bruto
+        valor_final = valor_bruto - valor_desconto
+
+        print(f"Desconto aplicado: {percentual_desc}%")  
+        print(f"Valor do desconto: R$ {valor_desconto:.2f}")
+        print(f"Valor final da venda: R$ {valor_final:.2f}")
+
+        total_vendas += 1
+        total_bruto += valor_bruto
+        total_descontos += valor_desconto
+        total_liquido += valor_final
     
    
 
