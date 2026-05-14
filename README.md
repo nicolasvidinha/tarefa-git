b7c7c39aed3b3bc5bc88d4896e324d4d16ebc4a4
Nicolas Vidinha <nicolasvidinha2@gmail.com>
2026-05-13 21:29:33 -0300

    implementa cadastro de venda

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-ECHO est� ativado.
+# Prova Prática - Fundamentos de Programação
+
+Nome do aluno:
+
+Descrição:
+Sistema simples para registro de vendas de uma loja.
+
+Como executar:
+python main.py
+
+# Lista de comandos no git executados
--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-ECHO est� ativado.
+total_vendas = 0
+total_bruto = 0
+total_descontos = 0
+total_liquido = 0
+
+while True:
+    print("\n=== SISTEMA DE VENDAS ===")
+    print("1 - Registrar venda")
+    print("2 - Ver resumo parcial")
+    print("3 - Encerrar sistema")
+
+    opcao = input("\n Escolha uma opção: ")
+
+    
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
